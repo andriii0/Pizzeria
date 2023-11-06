2a8482ed5b6a13f6bfec0df1d8a3bbb211a5cd53
andriii0 <531789@student.fontys.nl>
2023-11-06 15:11:04 +0100

    1111

--- main.py
+++ main.py
@@ -1,45 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, jsonify
+import json
+
 
 app = Flask(__name__)
+app.secret_key = 'Mario&Luigi!'
 
-products = [
 
-    #all the images are 800x800. To fill the webpage just google "product name" 800x800
-    #pizzas
-    {"name": "Pizza Margarita", "type": "Pizza", "section": "Vegetarian", "price": 10, "image": "margarita.jpg"},
-    {"name": "Pizza Salami", "type": "Pizza", "section": "Meat", "price": 15, "image": "salami.jpg"},
-    {"name": "Pizza Diavollo", "type": "Pizza", "section": "Meat", "price": 12, "image": "diavollo.jpg"},
-    {"name": "Pizza Tuna", "type": "Pizza", "section": "Meat", "price": 10, "image": "tuna.jpg"},
-    {"name": "Pizza Bolonieze", "type": "Pizza", "section": "Meat", "price": 10, "image": "bolonieze.jpg"},
-    {"name": "Pizza Speciale", "type": "Pizza", "section": "Meat", "price": 10, "image": "speciale.jpg"},
-    {"name": "Pizza Hawaii", "type": "Pizza", "section": "Meat", "price": 10, "image": "hawaii.jpg"},
-    {"name": "Pizza Quadro Formacci", "type": "Pizza", "section": "Vegetarian", "price": 10,
-     "image": "quadroformacci.jpg"},
-    {"name": "Pizza Quadro Stagione", "type": "Pizza", "section": "Vegetarian", "price": 10,
-     "image": "quadrostagione.jpg"},
-    {"name": "Pizza Francesko", "type": "Pizza", "section": "Vegetarian", "price": 10, "image": "francesko.jpg"},
 
-    {"name": "Coca Cola", "type": "Drinks", "section": "Cold", "price": 6, "image": "cocacola.jpg"},
-    {"name": "Pepsi", "type": "Drinks", "section": "Cold", "price": 5, "image": "pepsi.jpg"},
-    {"name": "Sprite", "type": "Drinks", "section": "Cold", "price": 5, "image": "sprite.jpg"},
-    {"name": "Fanta", "type": "Drinks", "section": "Cold", "price": 5, "image": "fanta.jpg"},
-    {"name": "Lemonade", "type": "Drinks", "section": "Cold", "price": 4, "image": "lemonade.jpg"},
-    {"name": "Iced Tea", "type": "Drinks", "section": "Cold", "price": 4, "image": "icedtea.jpg"},
-    {"name": "Orange Juice", "type": "Drinks", "section": "Cold", "price": 7, "image": "orangejuice.jpg"},
-    {"name": "Apple Juice", "type": "Drinks", "section": "Cold", "price": 7, "image": "applejuice.jpg"},
-    {"name": "Mineral Water", "type": "Drinks", "section": "Cold", "price": 3, "image": "mineralwater.jpg"},
-    {"name": "Milkshake", "type": "Drinks", "section": "Cold", "price": 8, "image": "milkshake.jpg"}
+with open('static/menu.json', 'r') as json_file:
+    menu_data = json.load(json_file)
 
-# we can add more products or categories
-]
-
-orders = []
+for product in menu_data['menu']:
+    name = product['name']
+    product_type = product['type']
+    section = product['section']
+    price = product['price']
+    image = product['image']
+    description = product['desc']
+#all the images are 800x800. To fill the webpage just google "product name" 800x800
 
 @app.route('/')
 def index():
-    return render_template('index.html', products=products, orders=orders)
+    cart_items = session.get('cart', {}).values()
+    total_price = sum(item['price'] * item['quantity'] for item in cart_items)
+    return render_template('index.html', products=menu_data['menu'], cart_items=cart_items, total_price=total_price)
+
+@app.route('/cart')
+def cart():
+    cart_items = session.get('cart', {}).values()
+    total_price = sum(item['price'] * item['quantity'] for item in cart_items)
+    return render_template('cart.html', cart_items=cart_items, total_price=total_price)
+
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    data = request.get_json()
+    product_name = data.get('name')
+    price = data.get('price')
+
+    cart = session.get('cart', {})
+
+    # If product is already in cart, update the quantity
+    if product_name in cart:
+        cart[product_name]['quantity'] += 1
+    else:
+        # If product is not in cart, add it as a dictionary
+        cart[product_name] = {'price': price, 'quantity': 1, 'name': product_name}
+
+    session['cart'] = cart
+
+    total_amount = sum(item['price'] * item['quantity'] for item in cart.values())
+    return jsonify(cart=session['cart'], totalAmount=total_amount)
+
+@app.route('/process_order', methods=['POST'])
+def process_order():
+    data = request.get_json()
+    payment_method = data.get('paymentMethod')
+    ordered_items = data.get('orderedItems')
+
+    for item in ordered_items:
+        item_name = item['name']
+        item_quantity = item['quantity']
+        item_price = item['price']
+        print(f"Item: {item_name}, Quantity: {item_quantity}")
+
+    # Process the payment and handle other order-related tasks here
+
+    return jsonify(message="Order processed successfully")
+@app.route('/clear_cart', methods=['POST'])
+def clear_cart():
+    session['cart'] = {}
+    total_amount = 0
+    return jsonify(cart=session['cart'], totalAmount=total_amount)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-#12:10 01.11.2023 version
--- static/script.js
+++ static/script.js
@@ -3,7 +3,9 @@ function filterCategory(category, subsection) {
     for (var i = 0; i < products.length; i++) {
         var product = products[i];
         var productCategory = product.classList.contains('Pizza') ? 'Pizza' : 'Drinks';
-        var productSubsection = product.classList.contains('Meat') ? 'Meat' : 'Vegetarian'; // Assuming there are only two subsections: Meat and Vegetarian
+        var productSubsection = product.classList.contains('Pizza') ?
+                                (product.classList.contains('Meat') ? 'Meat' : 'Vegetarian') :
+                                (product.classList.contains('Hot') ? 'Hot' : 'Cold');
 
         if ((category === 'all' || productCategory === category) &&
             (subsection === 'all' || productSubsection === subsection)) {
@@ -12,16 +14,145 @@ function filterCategory(category, subsection) {
             product.style.display = 'none';
         }
     }
+
+    var pizzasubcategories = document.querySelector('.subcategories-pizzas');
+    var drinkssubcategories = document.querySelector('.subcategories-drinks');
+
+    if (category === 'Pizza') {
+        pizzasubcategories.style.display = 'block';
+        drinkssubcategories.style.display = 'none';
+    } else if (category === 'Drinks') {
+        pizzasubcategories.style.display = 'none';
+        drinkssubcategories.style.display = 'block';
+    } else {
+        pizzasubcategories.style.display = 'none';
+        drinkssubcategories.style.display = 'none';
+    }
 }
+
 function addToCart(productName, price) {
-    var cartList = document.getElementById('cart-list');
-    var totalAmountElement = document.getElementById('total-amount');
-    var totalAmount = parseFloat(totalAmountElement.textContent);
-    totalAmount += price;
-    totalAmountElement.textContent = totalAmount.toFixed(2);
+    var xhr = new XMLHttpRequest();
+    var url = "/add_to_cart?cache=" + new Date().getTime();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
 
-    var listItem = document.createElement('li');
-    listItem.textContent = productName + ' - $' + price;
-    cartList.appendChild(listItem);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            updateCartUI(response.cart, response.totalAmount);
+        }
+    };
+
+    var data = {
+        name: productName,
+        price: price
+    };
+
+    xhr.send(JSON.stringify(data));
 }
 
+function clearCart() {
+    var xhr = new XMLHttpRequest();
+    var url = "/clear_cart?cache=" + new Date().getTime();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            updateCartUI(response.cart, response.totalAmount);
+        }
+    };
+
+    xhr.send();
+}
+
+function updateCartUI(cartItems, totalAmount) {
+    var cartList = document.querySelector('.cart-panel ul');
+    var totalAmountElement = document.querySelector('.cart-panel p');
+
+    cartList.innerHTML = '';
+    for (var productName in cartItems) {
+        if (cartItems.hasOwnProperty(productName)) {
+            var item = cartItems[productName];
+            var listItem = document.createElement('li');
+            listItem.textContent = item.name + ' - $' + item.price + ' x' + item.quantity;
+            cartList.appendChild(listItem);
+        }
+    }
+
+    totalAmountElement.textContent = 'Total amount: $' + totalAmount.toFixed(2);
+}
+
+function redirectToCheckout() {
+    window.location.href = '/cart';
+}
+
+function redirectToMenu() {
+    window.location.href = '/';
+}
+
+function openPaymentModal() {
+    var modal = document.getElementById('payment-modal');
+    modal.style.display = 'block';
+}
+
+var closeButton = document.querySelector('.close');
+closeButton.addEventListener('click', function() {
+    var modal = document.getElementById('payment-modal');
+    modal.style.display = 'none';
+});
+
+function processPayment(method) {
+    var modal = document.getElementById('payment-modal');
+    modal.style.display = 'none';
+
+    var orderedItems = [];
+    var cartItems = document.querySelectorAll('.cart-panel ul li');
+    cartItems.forEach(function (cartItem) {
+        var itemDetails = cartItem.textContent.split(' - ');
+        var itemName = itemDetails[0];
+        var itemPrice = parseFloat(itemDetails[1].split(' $')[1]);
+        var itemQuantity = parseInt(itemDetails[1].split(' x')[1]);
+        orderedItems.push({ name: itemName, price: itemPrice, quantity: itemQuantity });
+    });
+
+    var data = {
+        paymentMethod: method,
+        orderedItems: orderedItems
+    };
+
+    var xhr = new XMLHttpRequest();
+    var url = "/process_order";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log('Order processed successfully:', xhr.responseText);
+        }
+    };
+
+    xhr.send(JSON.stringify(data));
+}
+
+
+
+var modal = document.getElementById('payment-modal');
+var closeButton = modal.querySelector('.close');
+closeButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
+
+function closePaymentModal() {
+    var closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', closePaymentModal);
+    var modal = document.getElementById('payment-modal');
+    modal.style.display = 'none';
+}
